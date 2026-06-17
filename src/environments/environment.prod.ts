8f3a2603b668e473db7d84b0d507ab1fe73bbcc2
Mateoilr <milituma@sudamericano.edu.ec>
2026-06-17 17:04:00 -0500

    variable update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 // Producción - Vercel
 export const environment = {
   production: true,
-  apiUrl: 'https://zqjbpp54-3000.use2.devtunnels.ms/api/docs'
+  apiUrl: (import.meta as any).env?.NG_APP_API_URL || ''
 };
