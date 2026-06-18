f022275c5853092887431e597a43015f4445a277
Mateoilr <milituma@sudamericano.edu.ec>
2026-06-18 16:30:06 -0500

    chatbot desactivar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ChatWidgetComponent } from './chatbot/components/chat-widget/chat-widget.component';
+// import { ChatWidgetComponent } from './chatbot/components/chat-widget/chat-widget.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ChatWidgetComponent],
+  imports: [RouterOutlet/*, ChatWidgetComponent*/],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
