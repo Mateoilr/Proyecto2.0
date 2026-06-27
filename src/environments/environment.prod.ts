c87ddf07886783a69fa73936f0250e2456a94c50
Jeffrey-cel225 <milituma@sudamericano.edu.ec>
2026-06-27 09:01:32 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 // Producción - Vercel
 export const environment = {
   production: true,
-  apiUrl: 'https://backendlaboratorioag-production.up.railway.app/api'
+  apiUrl: 'https://backendlaboratorioag-production-98f1.up.railway.app/api'
 };
